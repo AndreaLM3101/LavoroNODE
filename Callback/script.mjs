28dbc9d6eb8cd8d9660f63b85498674d5e2a90b3
Andrea <andrealomuto31@gmail.com>
2025-04-02 00:55:47 +0200

    Lavoro Callback e Promise

---
+++ Callback/script.mjs
@@ -0,0 +1,21 @@
+import { writeFile } from 'node:fs'; // Importa solo la funzione writeFile dal modulo 'fs' (File System)
+import { Buffer } from 'node:buffer'; // Importa il modulo Buffer per lavorare con dati
+
+const data = new Uint8Array(Buffer.from('Hello Node.js')); // Buffer converte in dati // Uint8Array Converte in un array
+
+writeFile('message.txt', data, (err) => { // Scrive i dati nel file 'message.txt'
+    if (err) throw err; // Se c'è un errore, lo lancia e interrompe il programma
+    console.log('The file has been saved!');
+});
+
+
+
+  // import * as fs from 'node:fs'; // IMPORTA FILE SYSTEM PER LAVORARE AI FILE
+// import { Buffer } from 'node:buffer';
+
+// const data = new Uint8Array(Buffer.from('Hello Node.js'));
+
+// fs.writeFile('message.txt', data, (err) => {
+//  if (err) throw err;
+// console.log('The file has been saved!');
+// });
